12b8ca5ce25b7031b299c28b273d1075707af925
jjensen1453 <finch749@gmail.com>
2026-06-01 14:42:59 -0400

    server code for ubuntu

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001;
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || ['https://bostonshowtime.com', 'https://www.bostonshowtime.com', 'http://localhost:3000'],
   credentials: true
 }));
 
